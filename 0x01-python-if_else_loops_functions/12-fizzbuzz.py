964bf02e4661383366a86442abc1271e865d504c
JonnyCash <jonathanacp93@gmail.com>
2019-09-12 13:44:17 -0500

    digit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,4 +7,4 @@ for i in range(1, 101):
     elif i % 5 == 0:
         print("{}".format("Buzz"), end=" ")
     else:
-        print("{}".format(i), end=" ")
+        print("{:d}".format(i), end=" ")
