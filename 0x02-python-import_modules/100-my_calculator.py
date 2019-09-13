cdf90fc6d2e58d0f13714f85820d093b650068e7
JonnyCash <jonathanacp93@gmail.com>
2019-09-13 17:32:12 -0500

    update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+    import sys
     from calculator_1 import add, sub, mul, div
+
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     operators = [
             ["+", add],
             ["-", sub],
             ["*", mul],
             ["/", div]
             ]
-
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     operator = sys.argv[2]
 
     for opr in operators[:]:
-        if opr[0] == operator:
-            print("{} {} {} = {}".format(sys.argv[1],
-                                         sys.argv[2],
-                                         sys.argv[3],
-                                         opr[1](int(sys.argv[1]),
-                                         int(sys.argv[3]))))
+        if ord(opr[0]) == ord(operator):
+            print("{} {} {} = {}".format(a, operator, b, opr[1](a, b)))
             exit(0)
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
