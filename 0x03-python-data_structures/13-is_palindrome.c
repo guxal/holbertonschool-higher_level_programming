0df4064e69e087d28295b12e051f4f7d605e3723
JonnyCash <jonathanacp93@gmail.com>
2019-09-18 00:10:45 -0500

    add 13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * *add_nodeint - add node integer
+ * @head: head
+ * @n: new integer
+ * Return: listint_t
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+
+}
+/**
+ * is_palindrome - is palindrome
+ * @head: header
+ * Return: integer
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *new = NULL;
+	listint_t *tmp;
+
+	tmp = *head;
+
+	if (head == NULL)
+		return (0);
+
+	if (tmp->next == NULL)
+		return (0);
+
+	while (tmp)
+	{
+		new = add_nodeint(&new, tmp->n);
+		if (tmp->n == (tmp->next)->n)
+		{
+			tmp = tmp->next;
+			break;
+		}
+		tmp = tmp->next;
+	}
+
+	while (tmp)
+	{
+		if (tmp->n != new->n)
+			return (0);
+		tmp = tmp->next;
+		new = new->next;
+	}
+
+	return (1);
+}
