164cfaba4f75ca614f115a7b51e5b4cc0e590d52
JonnyCash <jonathanacp93@gmail.com>
2019-09-18 13:42:02 -0500

    hotfix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -30,12 +30,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 int is_palindrome(listint_t **head)
 {
 	listint_t *new, *tmp, *clear;
+	int _break = 1;
 
 	new = NULL, tmp = *head;
 	if (!head || !(*head) || !(*head)->next)
 		return (1);
-
-	while (tmp->next != NULL)
+	while (tmp->next != NULL || _break)
 	{
 		if (add_nodeint(&new, tmp->n) == NULL)
 			return (0);
@@ -43,9 +43,13 @@ int is_palindrome(listint_t **head)
 		{
 			tmp = tmp->next;
 			break;
+		}
+		if (tmp->n == ((tmp->next)->next)->n)
+		{
+			tmp = (tmp->next)->next;
+			break;
 		} tmp = tmp->next;
-	}
-	clear = new;
+	} clear = new;
 	if (tmp->next == NULL)
 	{
 		free_listint(clear);
@@ -57,14 +61,12 @@ int is_palindrome(listint_t **head)
 		{
 			free_listint(clear);
 			return (0);
-		}
-		tmp = tmp->next, new = new->next;
+		} tmp = tmp->next, new = new->next;
 	}
 	if (new->next == NULL)
 	{
 		free_listint(clear);
 		return (1);
-	}
-	free_listint(clear);
+	} free_listint(clear);
 	return (0);
 }
