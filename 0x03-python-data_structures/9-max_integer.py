6ae655cbdffebf514668b9211dc78af2efa0d75a
JonnyCash <jonathanacp93@gmail.com>
2019-09-17 20:02:10 -0500

    change conditions and equal variable

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (len(my_list) == 0):
+    if len(my_list) is 0:
         return (None)
-    _max = 0
+    _max = my_list[0]
     for i in my_list[:]:
         if (_max < i):
             _max = i
