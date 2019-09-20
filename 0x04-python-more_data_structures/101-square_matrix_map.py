35b964a2620ba0c712e6bd39b06c3369bea29ec7
JonnyCash <jonathanacp93@gmail.com>
2019-09-20 15:41:04 -0500

    change return with 0

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     if matrix is None:
-        return None
+        return 0
     return list(map(lambda e: list(map(lambda i: i**2, e)), matrix))
