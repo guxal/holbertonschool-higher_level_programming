c633d38f4d1d8c766f97f3461549c2da216767ef
guxal <jonathanacp93@gmail.com>
2019-10-05 15:00:00 +0000

    add docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Class definition: Square
+    """
     def __init__(self, size):
+        """
+        Args:
+            size: square size
+        """
         self.__size = size
