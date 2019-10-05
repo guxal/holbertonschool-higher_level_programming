6ef763827daa14757c16624e2e8d72931ec1b16f
guxal <jonathanacp93@gmail.com>
2019-10-05 16:13:35 +0000

    add docstrings

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Class definition: Square
 
+    Attributes:
+        __size (int): square size private
+
+    """
     def area(self):
+        """ Function area
+
+        Returns:
+            int: area the square
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """Function my_print print the square
+
+        Returns:
+            str: string square with #
+        """
         if self.size is 0:
             print("")
         else:
@@ -18,10 +34,21 @@ class Square:
 
     @property
     def size(self):
+        """
+        int: __size getter method
+        """
         return self.__size
 
     @size.setter
     def size(self, size):
+        """int: setter method in the __size
+        Args:
+            size: square size
+
+        Raises:
+            TypeError: if size is not type int
+            ValueError: if size is negative
+        """
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         elif int(size) >= 0:
