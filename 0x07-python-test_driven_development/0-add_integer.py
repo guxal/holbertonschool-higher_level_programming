ca4f510612f80203466c44a7575634e573e8a81f
guxal <jonathanacp93@gmail.com>
2019-10-05 22:58:18 +0000

    add 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ add two integers
+    """
+
+    if(isinstance(a, int) is False and isinstance(a, float) is False):
+        raise TypeError("a must be an integer")
+
+    if(isinstance(b, int) is False and isinstance(b, float) is False):
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
