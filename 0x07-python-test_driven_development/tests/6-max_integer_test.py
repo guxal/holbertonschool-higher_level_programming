b2e9346051d3eabf6929d5ff0b4c4340e8f778ee
guxal <jonathanacp93@gmail.com>
2019-10-06 01:23:20 +0000

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,25 +7,28 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_int(self):
-        self.assertAlmostEqual(max_integer([3, 2, 0]), 3)
+        self.assertEqual(max_integer([3, 2, 0]), 3)
 
     def test_list_str(self):
         self.assertRaises(TypeError, max_integer, [1, 2, "a"])
 
     def test_max_int_end(self):
-        self.assertAlmostEqual(max_integer([1, 4, 8]), 8)
+        self.assertEqual(max_integer([1, 4, 8]), 8)
 
     def test_max_int_middle(self):
-        self.assertAlmostEqual(max_integer([1, 10, 2]), 10)
+        self.assertEqual(max_integer([1, 10, 2]), 10)
 
     def test_int_negative(self):
-        self.assertAlmostEqual(max_integer([1, -10, 2]), 2)
+        self.assertEqual(max_integer([1, -10, 2]), 2)
 
     def test_int_all_negative(self):
-        self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
 
     def test_list_empty(self):
-        self.assertRaises(TypeError, max_integer, None)
+        self.assertEqual(max_integer([]), None)
 
     def test_list_one_element(self):
-        self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1]), 1)
+
+    if __name__ == '__main__':
+            unittest.main()
