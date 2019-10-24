c223aea6e6da13e186c0b9eb1c9b4b5f0285d904
guxal <jonathanacp93@gmail.com>
2019-10-24 19:22:23 +0000

    add 6-from_json

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+This method contains the function from_json_string
+"""
+
+
+def from_json_string(my_str):
+    """
+    From JSON to Object
+    """
+    return json.loads(my_str)
