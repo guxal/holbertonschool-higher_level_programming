7e21bbe4d80553ff1a5950b6aeafdb3d74be56f8
guxal <jonathanacp93@gmail.com>
2019-10-24 19:10:44 +0000

    add 5-to_json

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+This module contains the function to_json_string.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Return JSON.
+    """
+    return json.dumps(my_obj)
