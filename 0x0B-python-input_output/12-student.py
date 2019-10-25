b1cc41d86b029601ecd924fd71f6b34892a0ff5e
guxal <jonathanacp93@gmail.com>
2019-10-25 02:21:35 +0000

    add 12-student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the Class Student.
+"""
+
+
+class Student:
+    """
+    Class Student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializer new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Use magic method __dict__ return representation of class in json
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            ditc = self.__dict__
+            return{z: ditc[z] for ot in attrs for z in ditc if z == ot}
