3ca712a26362d99b7d596ad394edcf454c9b4d8e
guxal <jonathanacp93@gmail.com>
2019-10-27 19:55:03 +0000

    add exceptions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,26 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """init method"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def integer_validator(self, name, value):
+        """The function contains integer_validator
+        Args:
+            @name: name of error.
+            @value: integer validate.
+        Raises:
+            @TypeError: {name} must be an integer.
+            @ValueError: {name} must be > 0.
+        """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+
+        if value <= 0 and name not in ["x", "y"]:
+            raise ValueError(name + " must be > 0")
+        elif value < 0:
+            raise ValueError(name + " must be => 0")
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,20 @@ from models.base import Base
 
 class Rectangle(Base):
     """
-    This Class Rectangle
+    This Class Rectangle inherit from Base.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        init method
+        Args:
+            @width: width rectangle.
+            @height: height rectangle.
+            @x: x position
+            @y: y position
+        """
         super().__init__(id)
+
         self.width = width
         self.height = height
         self.x = x
@@ -19,32 +28,64 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter width
+        Attributes:
+            @__width: rectangle width
+        Args:
+            @value: rectangle width value
+        """
+        self.integer_validator("width", value)
         self.__width = value
 
     @property
     def height(self):
+        """getter height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter height
+        Attributes:
+            @__height: rectangle height
+        Args:
+            @value: rectangle height value
+        """
+        self.integer_validator("height", value)
         self.__height = value
 
     @property
     def x(self):
+        """getter x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter x
+        Attributes:
+            @__x: x position
+        Args:
+            @value: x position value
+        """
+        self.integer_validator("x", value)
         self.__x = value
 
     @property
     def y(self):
+        """getter y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter y
+        Attributes:
+            @__y: y position
+        Args:
+            @value: y position value
+        """
+        self.integer_validator("y", value)
         self.__y = value
