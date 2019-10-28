24488c17a7eaa2238d46f0e44279ce45576c441a
guxal <jonathanacp93@gmail.com>
2019-10-28 00:45:23 +0000

    update pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,7 +48,6 @@ class Rectangle(Base):
                 if idx == 4:
                     self.y = arg
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         init method
