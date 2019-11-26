34628a3a05fcf7dfb72e634d8ac58b1eb0415a66
guxal <jonathanacp93@gmail.com>
2019-11-26 23:23:23 +0000

    update task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- ALL privileger for the user_0d_1
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'holbipass';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
