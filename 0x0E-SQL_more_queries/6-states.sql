815518a6e2b8cdd112c3e86393063d93db73a285
guxal <jonathanacp93@gmail.com>
2019-11-27 04:00:45 +0000

    new comment

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- create table unique_id use DEFAULT and UNIQUE Constraint
+-- create database and new table.
 CREATE database IF NOT EXISTS hbtn_0d_usa; 
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id int NOT NULL UNIQUE AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id));
