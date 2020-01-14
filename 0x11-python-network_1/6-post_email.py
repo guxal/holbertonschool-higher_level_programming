dc86bb13ec49f2fdee7512f6058d9ff7204dba0b
guxal <jonathanacp93@gmail.com>
2020-01-14 17:46:29 +0000

    add 6_post_email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""Request Headers"""
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
