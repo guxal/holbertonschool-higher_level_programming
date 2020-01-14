57c19aece7ee1804f6d2c5d7b2198f15afdb2ce1
guxal <jonathanacp93@gmail.com>
2020-01-14 06:11:11 +0000

    hotfiz

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 import sys
-"""Request with Request"""
+"""Request Headers"""
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     print(r.headers['X-Request-Id'])
