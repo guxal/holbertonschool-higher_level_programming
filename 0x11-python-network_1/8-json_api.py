4c72b47f850012cb8fa3854aef65403a8b4538b7
guxal <jonathanacp93@gmail.com>
2020-01-14 23:03:29 +0000

    add 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,15 +6,19 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         ul2 = "http://0.0.0.0:5000/search_user"
         url = "http://5c332d577923.41.hbtn-cod.io:5000/search_user"
-        r = requests.post(url2, data={'q': sys.argv[1]})
+        try:
+            d = {'q': sys.argv[1]}
+        except Exception:
+            d = {'q': ""}
+
+        r = requests.post(url, data={'q': sys.argv[1]})
         try:
             json = r.json()
+            try:
+                print("["+str(json['id'])+"]"+" "+json['name'])
+            except KeyError:
+                print("No result")
         except Exception:
             print("Not a valid JSON")
-
-        try:
-            print("["+str(json['id'])+"]"+" "+json['name'])
-        except KeyError:
-            print("No result")
     else:
         print("No result")
