93399b467b2b1861c9f7a603f1740c0e110ef8fd
guxal <jonathanacp93@gmail.com>
2020-01-14 06:00:33 +0000

    add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""Request with Request"""
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: %s" % type(r.text))
+    print("\t- content: %s" % r.text)
