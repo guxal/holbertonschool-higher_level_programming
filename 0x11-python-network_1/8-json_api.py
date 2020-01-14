5c3c8fdbc95f6ebbfa8c7454c4871438b5f91a18
guxal <jonathanacp93@gmail.com>
2020-01-14 20:28:58 +0000

    change url

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         ul2 = "http://0.0.0.0:5000/search_user"
         url = "http://5c332d577923.41.hbtn-cod.io:5000/search_user"
-        r = requests.post(url, data={'q': sys.argv[1]})
+        r = requests.post(url2, data={'q': sys.argv[1]})
         try:
             json = r.json()
         except Exception:
