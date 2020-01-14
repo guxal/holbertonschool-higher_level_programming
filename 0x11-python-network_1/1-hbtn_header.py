dbbf9ca8dd10bf3c4959719550d971d29ee5d506
guxal <jonathanacp93@gmail.com>
2020-01-14 01:42:15 +0000

    hotfix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from urllib import request
+import sys
 """Request header"""
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen(sys.argv[1]) as response:
         header = response.info().get('X-Request-Id')
         print(header)
