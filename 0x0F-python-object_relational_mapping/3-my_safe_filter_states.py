83d19552de40f7daa22a159d46e4691d7c0f06f7
guxal <jonathanacp93@gmail.com>
2020-01-21 00:21:59 +0000

    add 3-my_safe_filter_states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''This script list states where name matches the argv[4] the way safely'''
+import sys
+import MySQLdb
+
+if len(sys.argv) == 5:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    mydb = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        database=database
+    )
+
+    mycursor = mydb.cursor()
+    query = "SELECT * FROM states WHERE name= %(username)s"
+    mycursor.execute(query,{'username': name})
+    myresult = mycursor.fetchall()
+
+    for data in myresult:
+        print(data)
+
+    mycursor.close()
