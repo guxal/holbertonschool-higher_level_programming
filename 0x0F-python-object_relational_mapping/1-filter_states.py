5a49f01794273cb63348d82c4539f11f678f6688
guxal <jonathanacp93@gmail.com>
2020-01-21 00:19:55 +0000

    add 1-filter_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''This script list states with a name starting with N (upper N)'''
+import sys
+import MySQLdb
+
+if len(sys.argv) == 4:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    mydb = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        database=database
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM states WHERE (name REGEXP '^N')")
+    myresult = mycursor.fetchall()
+
+    for data in myresult:
+        print(data)
+
+    mycursor.close()
