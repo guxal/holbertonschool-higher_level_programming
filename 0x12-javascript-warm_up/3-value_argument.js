dfc1714cd8587ac9e5a6f8fabf6aa87ab252f893
guxal <jonathanacp93@gmail.com>
2020-02-04 18:48:22 +0000

    add task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function print (value) {
+  console.log(value);
+}
+
+let idx = 0;
+process.argv.forEach((val, index) => {
+  idx = index;
+});
+
+if (idx === 2) { print(process.argv[idx]); } else if (idx < 2) { print('No argument'); } else { print('Arguments found'); }
