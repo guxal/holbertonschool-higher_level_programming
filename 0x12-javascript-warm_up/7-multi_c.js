1305380c5fefe0bb552863a874e258aa76d239a1
guxal <jonathanacp93@gmail.com>
2020-02-04 19:48:57 +0000

    changes logic

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,14 +3,9 @@ function print (value) {
   console.log(value);
 }
 
-let idx = 0;
-process.argv.forEach((val, index) => {
-  idx = index;
-});
-
 const argv = process.argv;
 
-if (idx >= 2) {
+if (argv.length >= 3) {
   if (argv[2] > 0) {
     let i;
     for (i = 0; i < argv[2]; i++) { print('C is fun'); }
