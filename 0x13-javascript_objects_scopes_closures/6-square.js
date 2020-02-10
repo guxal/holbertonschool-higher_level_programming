0199f0fc406a9eccbc45ac8619da1a76176f094e
guxal <jonathanacp93@gmail.com>
2020-02-10 21:43:57 +0000

    add if

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,6 +8,9 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
+    if (this.width === undefined) {
+      return;
+    }
     for (let i = 0; i < this.width; i++) {
       if (c !== undefined) {
         console.log(c.repeat(this.width));
