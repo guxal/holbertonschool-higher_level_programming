0ab2112892c7b19196bad37346c01ee3e9217288
guxal <jonathanacp93@gmail.com>
2020-02-10 21:42:03 +0000

    add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.width; i++) {
+      if (c !== undefined) {
+        console.log(c.repeat(this.width));
+      } else {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
