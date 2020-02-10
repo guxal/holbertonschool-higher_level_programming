086a5a137074ca08724c939e1f8b7e4282110089
guxal <jonathanacp93@gmail.com>
2020-02-10 22:17:08 +0000

    add task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,6 @@ exports.esrever = function (list) {
   const newlist = [];
   let count = 0;
   for (let i = list.length - 1; i >= 0; i--) {
-    console.log(list[i]);
     newlist[count] = list[i];
     count++;
   }
