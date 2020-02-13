b8b31a47940cdeaa1b5bb15ea9127abfe8e0c378
guxal <jonathanacp93@gmail.com>
2020-02-13 21:46:51 +0000

    add task 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,14 +1,9 @@
 function addClass (event) {
-  console.log('aqui');
   $('header').toggleClass(function () {
-    console.log(this);
-
     if ($(this).is('.red')) {
-      console.log('green');
       $(this).removeClass('red');
       return 'green';
     } else {
-      console.log('not green');
       $(this).removeClass('green');
       return 'red';
     }
